1ca2f6d1445d5cfb17f2ce3d35d5d69a8cfff979
Muniyandi Perumal <pmuniyandi@gmail.com>
2022-03-25 19:29:37 +0530

    Modified

--- datatype.c
+++ datatype.c
@@ -15,5 +15,5 @@ int main() {
     printf("%c %d %d %f %lf\n", c, s, i, f, d);
     printf("%x %x %x %x %x\n", &c, &s, &i, &f, &d);
     printf("%d %d %d %d %d %d %d %d %d\n", sizeof(char), sizeof(short), sizeof(long), sizeof(int), sizeof(float), sizeof(double), sizeof(long int), sizeof(long double), sizeof(long long int));
-}
+} 
 
