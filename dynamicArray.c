62ba6bf086b09ef4efb62d63dcbff744fa09b582
PythonTrainingSP <pythontraning737@gmail.com>
2022-05-28 12:25:16 +0530

    modified for user defined data

--- dynamicArray.c
+++ dynamicArray.c
@@ -1,17 +1,26 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
+struct doctor {
+    char name[20];
+    float fee;
+    char sl[20];
+   
+};
 
 struct darray {
-    int data;
-    struct darray *nxt;
+    struct doctor dobj; // data part user defined obj instead of int
+    struct darray *nxt; // link
 };
+
+
 struct darray *dlist = NULL;
 
 void listArray() {
     struct darray *head = dlist;
     printf("[");
     while(head != NULL)  {
-        printf("%d",head->data);
+        printf("(%s,%s,%f)",head->dobj.name, head->dobj.sl, head->dobj.fee);
         if(head->nxt!=NULL) {
             printf(",");
         } 
@@ -21,11 +30,13 @@ void listArray() {
   
 }
 
-void addElement(int data) {
+void addElement(struct doctor dobj) {
     struct darray *tptr = dlist;
 
     struct darray *node =  (struct darray *)calloc(1,sizeof(struct darray));
-    node->data = data;  // data assign to node -> data
+    strcpy(node->dobj.name, dobj.name); 
+    node->dobj.fee = dobj.fee; 
+    strcpy(node->dobj.sl, dobj.sl);  // data assign to node -> data
     node->nxt = NULL;
 
      if(tptr == NULL) {
@@ -41,10 +52,18 @@ void addElement(int data) {
 }
 
 int main() {
+    struct doctor d1,d2;
+    strcpy(d1.name, "Ravi");
+    d1.fee = 200;
+    strcpy(d1.sl, "asdf");
+
+    strcpy(d2.name, "Raj");
+    d2.fee = 1000;
+    strcpy(d2.sl, "pqr");
 
     listArray();
-    addElement(10);
-    addElement(20);
+    addElement(d1);
+    addElement(d2);
     listArray();
 }
 
